f5c4fd6e166f4a271361d63833d5eac40ec99fa5
Meng Liu <604629@gmail.com>
2019-04-15 14:54:33 +0800

    2019.4.15update

--- multigpu_train.sh
+++ multigpu_train.sh
@@ -2,11 +2,11 @@ python multigpu_train.py \
 	--gpu_list=0,1 \
 	--input_size=512 \
 	--batch_size_per_gpu=16 \
+        --pretrained_model_path=./checkpoints_train_MLT2017 \
 	--checkpoint_path=./checkpoints/ \
 	--text_scale=512 \
-	--training_data_path=../../Dataset/MLT2019/MLT_train+validation \
+	--training_data_path=../../Dataset/MLT2019/ICDAR2015_train\
 	--geometry=RBOX \
-	--learning_rate=0.0002 \
+	--learning_rate=0.00001 \
 	--num_readers=24 \
-        --restore = True
-	#--pretrained_model_path=./base_model/model.ckpt-516002 	# Here we don't adopt a pre-trained model. If you want to use the pre-trained model, you need download it yourself (Please refer to https://github.com/argman/EAST)
+        --restore=False 
